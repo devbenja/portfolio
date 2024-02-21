58051f677843dfac6196b09f5c38dcec43881859
Benjamin Carías <cariasbenjamin59@gmail.com>
2024-02-21 01:50:57 -0600

    Add Toggle Theme Change

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { NavBar } from "@/components/navbar";
+import { ToggleTheme } from "@/components/toggle-theme";
 
 export default function Home() {
   return (
     <main className="pb-40">
       <NavBar/>
+      <ToggleTheme/>
     </main>
   );
 }
+ 
